5a1351e81b1d62c841caaf99a34fd4088d23b0c1
ori mazor <pivot7700@walla.co.il>
2021-01-08 16:37:11 +0200

    added chat #120

---
+++ lib/widgets/loading.dart
@@ -0,0 +1,67 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:tech_pool/Utils.dart';
+
+class Loading extends StatelessWidget {
+  const Loading();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: CircularProgressIndicator(
+          valueColor: AlwaysStoppedAnimation<Color>(secondColor),
+        ),
+      ),
+      color: Colors.white.withOpacity(0.8),
+    );
+  }
+}
+
+
+class FullPhoto extends StatelessWidget {
+  final String url;
+
+  FullPhoto({Key key, @required this.url}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'FULL PHOTO',
+          style: TextStyle(color: primaryColor, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: FullPhotoScreen(url: url),
+    );
+  }
+}
+
+class FullPhotoScreen extends StatefulWidget {
+  final String url;
+
+  FullPhotoScreen({Key key, @required this.url}) : super(key: key);
+
+  @override
+  State createState() => FullPhotoScreenState(url: url);
+}
+
+class FullPhotoScreenState extends State<FullPhotoScreen> {
+  final String url;
+
+  FullPhotoScreenState({Key key, @required this.url});
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: PhotoView(imageProvider: CachedNetworkImageProvider(url)));
+  }
+}

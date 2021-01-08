f5c01a280012a5414aabdce23227927815660ab7
abir006 <39681215+abir006@users.noreply.github.com>
2021-01-08 21:05:09 +0200

    closed #118, closed #119.

--- lib/pages/FavoritesPage.dart
+++ lib/pages/FavoritesPage.dart
@@ -23,8 +23,168 @@ class _FavoritesPageState extends State<FavoritesPage> {
           stream: firestore.collection("Favorites").doc(userRep.user.email).snapshots(),
           builder: (context, snapshot) {
             if (snapshot.hasData) {
-              Map<String,Map<String,dynamic>> snapshotMap = Map<String,Map<String,dynamic>>.from(snapshot.data.data());
+              List<Widget> favorites = [];
+              Map<String,Map<String,dynamic>> snapshotMap;
+              if(snapshot.data.data() == null){
+                snapshotMap = {};
+              }else {
+                snapshotMap = Map<String,
+                    Map<String, dynamic>>.from(snapshot.data.data());
+              }
+              snapshotMap.forEach((key, value) {
+                final GlobalKey<PopupMenuButtonState> _menuKey = GlobalKey();
+                if(key != "Home" && key != "Work"){
+                  favorites.add(ListTile(onTap: () => _menuKey.currentState.showButtonMenu(),title: Text(key),
+                    subtitle: Text(value["Address"]),
+                    leading: Icon(Icons.favorite, size: 30,
+                      color: secondColor,),
+                    trailing:  PopupMenuButton(key: _menuKey,icon: Icon(Icons.more_vert),
+                        itemBuilder: (BuildContext context) => [PopupMenuItem(value: "Edit name",child: Row(children: [Icon(Icons.edit),Text("Edit name")],)),PopupMenuItem(value: "Edit location",child: Row(children: [Icon(Icons.edit_location),Text("Edit address")]),),PopupMenuItem(value: "Delete",child: Row(children: [Icon(Icons.delete),Text("Delete")]))],
+                        onSelected: (value2) async {
+                          if(value2 == "Delete"){
+                            snapshotMap.remove(key);
+                            await firestore.collection("Favorites").doc(userRep.user.email).set(snapshotMap);
+                          } else if (value2 == "Edit location") {
+                            var returnResult = await Navigator.of(context)
+                                .push(MaterialPageRoute<LocationsResult>(
+                                builder: (BuildContext context) {
+                                  return LocationSearch(
+                                    showAddStops: false, fromFavorites: true,);
+                                },
+                                fullscreenDialog: true));
+                            if (returnResult != null) {
+                              await firestore.collection("Favorites").doc(
+                                  userRep.user.email).update({
+                                key: {
+                                  "Address": returnResult.fromAddress
+                                      .addressLine,
+                                  "Point": GeoPoint(
+                                      returnResult.fromAddress.coordinates
+                                          .latitude,
+                                      returnResult.fromAddress.coordinates
+                                          .longitude)
+                                }
+                              });
+                            }
+                          }else if (value2 == "Edit name"){
+                            showDialog(context: context,builder: (_) {
+                              var _formKey = GlobalKey<FormState>();
+                              var _controller = TextEditingController();
+                              _controller.text = key;
+                              return AlertDialog(shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                                title: Text("Edit name"),content: Form(key: _formKey,child: TextFormField(decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),labelText: "Name"),controller: _controller,validator: (val) {
+                                  if (val.isEmpty) {return 'Please enter name';} else if(val.toLowerCase() == "home" || val.toLowerCase() == "work"){return 'Cant select this name';}else if(snapshotMap.containsKey(val)) {return 'Name already exists';}return null;
+                                },)),
+                                actions: [
+                                  TextButton(onPressed: () =>
+                                      Navigator.pop(context),
+                                      child: Text("Discard")),
+                                  TextButton(onPressed: () async {
+                                    if(_formKey.currentState.validate()) {
+                                      snapshotMap.remove(key);
+                                      snapshotMap[_controller.text] = value;
+                                      await firestore.collection("Favorites")
+                                          .doc(userRep.user.email)
+                                          .set(snapshotMap);
+                                      Navigator.pop(context);
+                                    }
+                                  },
+                                      child: Text("Confirm"))
+                                ],
+                              );
+                            });
+                          }
+                        }
+                    )
+                    /*IconButton(icon: Icon(Icons.more_vert),
+                          onPressed: () async {
+                            var returnResult = await Navigator.of(context)
+                                .push(MaterialPageRoute<LocationsResult>(
+                                builder: (BuildContext context) {
+                                  return LocationSearch(
+                                    showAddStops: false, fromFavorites: true,);
+                                },
+                                fullscreenDialog: true));
+                            if (returnResult != null) {
+                              await firestore.collection("Favorites").doc(userRep.user.email).update({key : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}});
+                            }
+                          },
+
+                        )*/,)
+                  );
+                  favorites.add(Divider(thickness: 1,indent: 10,endIndent: 10,color: mainColor,));
+                }
+              });
+              if(favorites.isNotEmpty) {
+                favorites.removeLast();
+              }
               return Scaffold(
+                  floatingActionButton: FloatingActionButton(heroTag: "add",
+                    backgroundColor: Colors.black,
+                    child: Icon(Icons.add, size: 30),
+                    onPressed: () {
+                      showDialog(context: context,builder: (_) {
+                        LocationsResult returnResult;
+                        final _formKey = GlobalKey<FormState>();
+                        var _controller = TextEditingController();
+                        return Scaffold(
+                            key: _key,
+                            backgroundColor: Colors.transparent,
+                            body: Builder(
+                                builder: (context) => AlertDialog(shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
+                                  title: Text("New favorite"),content: Form(key: _formKey,child: Wrap(runSpacing: 10,alignment: WrapAlignment.center,
+                                    children: [
+                                      TextFormField(decoration: InputDecoration(border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),labelText: "Name"),controller: _controller,validator: (val) {
+                                        if (val.isEmpty) {return 'Please enter name';} else if(val.toLowerCase() == "home" || val.toLowerCase() == "work"){return 'Cant select this name';}else if(snapshotMap.containsKey(val)) {return 'Name already exists';}return null;
+                                      },),
+                                      RaisedButton.icon(
+                                        color: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius: BorderRadius.circular(18),
+                                            side: BorderSide(color: Colors.black)),
+                                        label: Text("Choose location"),
+                                        icon: Icon(Icons.map),
+                                        onPressed: () async {
+                                          returnResult = await Navigator.of(context)
+                                              .push(MaterialPageRoute<LocationsResult>(
+                                              builder: (BuildContext context) {
+                                                return LocationSearch(showAddStops: false,fromFavorites: true,);
+                                              },
+                                              fullscreenDialog: true));
+                                        },
+                                      )
+                                    ],
+                                  )),
+                                  actions: [
+                                    TextButton(onPressed: () =>
+                                        Navigator.pop(context),
+                                        child: Text("Discard")),
+                                    TextButton(onPressed: () async {
+                                      if(_formKey.currentState.validate()) {
+                                        if (returnResult == null) {
+                                          _key.currentState.showSnackBar(SnackBar(
+                                              content: Text("Please select location")));
+                                        } else {
+                                          await firestore.collection("Favorites")
+                                              .doc(userRep.user.email)
+                                              .set({
+                                            _controller.text: {
+                                              "Address": returnResult.fromAddress.addressLine,
+                                              "Point": GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)
+                                            }
+                                          },SetOptions(merge: true));
+                                          Navigator.pop(context);
+                                        }
+                                      }
+                                    },
+                                        child: Text("Confirm"))
+                                  ],
+                                )));
+                      });
+                    },
+                  ),
                   backgroundColor: mainColor,
                   appBar: AppBar(
                     elevation: 0,
@@ -90,11 +250,11 @@ class _FavoritesPageState extends State<FavoritesPage> {
                               },
                               fullscreenDialog: true));
                           if (returnResult != null) {
-                            await firestore.collection("Favorites").doc(userRep.user.email).update({"Home" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}});
+                            await firestore.collection("Favorites").doc(userRep.user.email).set({"Home" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}},SetOptions(merge: true));
                           }
                         }
                       },title: Text("Home"),
-                        subtitle: Text((!snapshotMap.containsKey("Home") ? "Click edit to set home address" : snapshotMap["Home"]["Address"])),
+                        subtitle: Text((!snapshotMap.containsKey("Home") ? "Click to set home address" : snapshotMap["Home"]["Address"])),
                         leading: Icon(Icons.home, size: 30,
                           color: secondColor,),
                         trailing: snapshotMap.containsKey("Home") ? PopupMenuButton(key: _homeMenuKey,icon: Icon(Icons.more_vert),
@@ -137,12 +297,12 @@ class _FavoritesPageState extends State<FavoritesPage> {
                                 },
                                 fullscreenDialog: true));
                             if (returnResult != null) {
-                              await firestore.collection("Favorites").doc(userRep.user.email).update({"Home" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}});
+                              await firestore.collection("Favorites").doc(userRep.user.email).set({"Home" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}},SetOptions(merge: true));
                             }
                           },
 
                         ),),
-                      Divider(),
+                      Divider(thickness: 1,indent: 10,endIndent: 10,color: mainColor,),
                       ListTile(onTap: () async {
                         if(_workMenuKey.currentState != null){
                           _workMenuKey.currentState.showButtonMenu();
@@ -155,11 +315,11 @@ class _FavoritesPageState extends State<FavoritesPage> {
                               },
                               fullscreenDialog: true));
                           if (returnResult != null) {
-                            await firestore.collection("Favorites").doc(userRep.user.email).update({"Work" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}});
+                            await firestore.collection("Favorites").doc(userRep.user.email).set({"Work" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}},SetOptions(merge: true));
                           }
                         }
                       },title: Text("Work"),
-                        subtitle: Text((!snapshotMap.containsKey("Work") ? "Click edit to set work address" : snapshotMap["Work"]["Address"])),
+                        subtitle: Text((!snapshotMap.containsKey("Work") ? "Click to set work address" : snapshotMap["Work"]["Address"])),
                         leading: Icon(Icons.work, size: 30,
                           color: secondColor,),
                         trailing: snapshotMap.containsKey("Work") ? PopupMenuButton(key: _workMenuKey,icon: Icon(Icons.more_vert),
@@ -202,9 +362,11 @@ class _FavoritesPageState extends State<FavoritesPage> {
                                 },
                                 fullscreenDialog: true));
                             if (returnResult != null) {
-                              await firestore.collection("Favorites").doc(userRep.user.email).update({"Work" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}});
+                              await firestore.collection("Favorites").doc(userRep.user.email).set({"Work" : {"Address" : returnResult.fromAddress.addressLine,"Point" : GeoPoint(returnResult.fromAddress.coordinates.latitude,returnResult.fromAddress.coordinates.longitude)}},SetOptions(merge: true));
                             }
                           },),),
+                      Divider(thickness: 1,indent: 10,endIndent: 10,color: mainColor,),
+                      ...favorites
                     ],),
                   ));
             }else if(snapshot.hasError){
@@ -217,7 +379,12 @@ class _FavoritesPageState extends State<FavoritesPage> {
                   ]);
             }else{
               return Scaffold(
-                key: _key,
+                  key: _key,
+                  floatingActionButton: FloatingActionButton(heroTag: "add",
+                    backgroundColor: Colors.black,
+                    child: Icon(Icons.add, size: 30),
+                    onPressed: () {},
+                  ),
                   backgroundColor: mainColor,
                   appBar: AppBar(
                     elevation: 0,
